209e0ae23a09439fe777e4ee915c7719b5a36b50
Manh <hoangducmanhhust@gmail.com>
2024-03-29 07:35:27 +0700

    new_data

---
+++ new.py
@@ -0,0 +1,23 @@
+import os
+import time
+import uuid
+import cv2
+IMAGES_PATH = os.path.join('new')
+number_images = 50
+
+
+cap = cv2.VideoCapture(0)
+
+
+for imgnum in range(number_images):
+    print('Collecting image {}'.format(imgnum))
+    ret, frame = cap.read()
+    imgname = os.path.join(IMAGES_PATH,f'{imgnum}.jpg')
+    cv2.imwrite(imgname, frame)
+    cv2.imshow('frame', frame)
+    time.sleep(0.5)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
